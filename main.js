0477777b96346f368e38745c03648eb9f6470669
Mykyta Prokopiev <nikprokopev01@gmail.com>
2025-12-28 11:24:56 +0200

    commit

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ async function prepareImages(imagesList) {
 
             const offscreenCanvas = canvas.transferControlToOffscreen();
 
-            img.src = `./imageDrawing/images/${i + 1}.jpg`;
+            img.src = `./images/${i + 1}.jpg`;
             img.onload = () => {
                 createImageBitmap(img).then(bitmap => {
                     canvasWorker.postMessage(
